ba35c9a5f9a57f356b692b18763c06acafaecad6
JounQin <admin@1stg.me>
2019-07-19 21:00:45 +0800

    Create now-cli.rb

---
+++ Formula/now-cli.rb
@@ -0,0 +1,41 @@
+require "language/node"
+
+class NowCli < Formula
+  desc "The command-line interface for Now"
+  homepage "https://zeit.co/now"
+  url "https://github.com/zeit/now-cli/archive/15.8.4.tar.gz"
+  sha256 "554e6a54b410000ee8ec9a9d9bec662059460d51021a6fccafa095610cc91b33"
+
+  depends_on "node" => :build
+
+  def install
+    # don't run postinstall
+    inreplace("package.json") { |s| s.gsub! /^.*"postinstall".*$/, "" }
+
+    system "npm", "install", *Language::Node.local_npm_install_args
+    system "npm", "run", "build"
+
+    # Read the target node version from package.json
+    target = IO.read("package.json").match(/\"(node\d+(\.\d+){2})-[^"]+\"/)[1]
+
+    # This packages now-cli together with a patched version of node
+    pkg_args = %W[
+      --c=package.json
+      --o=now
+      --options=no-warnings
+      --targets=#{target}
+    ]
+    system "node_modules/.bin/pkg", "bin/now.js", *pkg_args
+
+    bin.install "now"
+  end
+
+  test do
+    system "#{bin}/now", "init", "bash"
+    assert_predicate testpath/"bash/index.sh", :exist?, "index.sh must exist"
+    assert_predicate testpath/"bash/now.json", :exist?, "now.json must exist"
+    assert_predicate testpath/"bash/README.md", :exist?, "README.md must exist"
+    system "echo", "handler >> bash/index.sh"
+    system "bash", "bash/index.sh"
+  end
+end
