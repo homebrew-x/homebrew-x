4acfecc555899e8340262736b7768640f21dfc0e
JounQin <admin@1stg.me>
2025-08-28 10:10:58 +0800

    chore: bump moonbit to 0.6.25

--- Formula/moonbit.rb
+++ Formula/moonbit.rb
@@ -6,7 +6,7 @@ class Moonbit < Formula
   desc 'An end-to-end programming language toolchain for cloud and edge computing using WebAssembly'
   homepage 'https://www.moonbitlang.com'
   license 'MIT'
-  version '0.6.24+012953835'
+  version '0.6.25+d6913262c'
 
   os = OS.mac? ? 'darwin' : 'linux'
   arch = Hardware::CPU.arm? ? 'aarch64' : 'x86_64'
@@ -18,14 +18,14 @@ class Moonbit < Formula
   # stree-ignore
   sha256 OS.mac? ?
     if Hardware::CPU.arm?
-      '3c965b8679ccbb2b707a4586a91645c24fe40033271019b99674bd4985c2574f'
+      '23e5ab5b46239b8a6f8eae0a0f1021fa241baeca586a3d64ecfa4f985ff9cbb1'
     else
       '5b74cee20fdc1c134c0fe0361cdbfce5b98cdc36cdaa105a52049916e7d88df5'
     end :
     if Hardware::CPU.arm?
       '' # unavailable for linux arm64
     else
-      '4def890a344a911a85e46f8c84b1e6904a28059ab840e7c9d8c4c5ecbae8c864'
+      'aa87247bcf1b516043b969a9ceea43566eeeb2f90980c1c6f6d9143e1c05dbea'
     end
 
   def install
