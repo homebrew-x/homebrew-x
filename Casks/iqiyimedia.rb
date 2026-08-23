e3c2e0da00bef70c5a60ba9ab514480d379bbd01
JounQin <admin@1stg.me>
2026-08-23 17:45:31 +0800

    chore: add cask bump workflow and update iqiyimedia

--- Casks/iqiyimedia.rb
+++ Casks/iqiyimedia.rb
@@ -1,5 +1,5 @@
 cask 'iqiyimedia' do
-  version '17.6.1,20260616180100'
+  version '17.8.0,20260821101500'
   sha256 :no_check
 
   url 'https://static-d.iqiyi.com/ext/common/iQIYIMedia_271.dmg'
@@ -13,7 +13,7 @@ cask 'iqiyimedia' do
     strategy :extract_plist
   end
 
-  depends_on macos: :catalina
+  depends_on macos: :big_sur
 
   app '爱奇艺.app'
 
